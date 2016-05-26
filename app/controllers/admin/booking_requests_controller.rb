6a7e658d0e6a201f6be113b9788a229b1f76a20e
Ben Barnett <ben@brushfiredesign.net>
2016-05-26 14:01:02 +0100

    Only show records with a first name

--- app/controllers/admin/booking_requests_controller.rb
+++ app/controllers/admin/booking_requests_controller.rb
@@ -2,6 +2,6 @@ class Admin::BookingRequestsController < ApplicationController
   layout 'full_width'
 
   def index
-    @booking_requests = BookingRequest.last(50).reverse
+    @booking_requests = BookingRequest.where.not(first_name: nil).last(50).reverse
   end
 end
