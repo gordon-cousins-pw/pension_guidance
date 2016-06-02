4fa0cfc830c3aaae53ec362e7cd149e0e9e5584a
Ben Barnett <ben@brushfiredesign.net>
2016-06-02 14:21:07 +0100

    More specific booking slots for TPAS prototype

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -6,6 +6,8 @@ class Slot < ActiveRecord::Base
   end
 
   def period
-    name == '0800-1200' ? 'Morning' : 'Afternoon'
+    'Morning' if name == '0800-1200'
+    'Afternoon' if name == '1300-1700'
+    "#{name[0,2].to_i > 12 ? (name[0,2].to_i - 12).to_s : name[0,2]}:#{name[2,2]}#{name[0,2].to_i > 11 ? 'pm' : 'am'}"
   end
 end
